569de7ee62c2412182cd928a23df5c2b37577165
Aaron Hubbard <hubbaraa@gmail.com>
2024-12-31 10:55:14 -0800

    first commit

--- importgp.py
+++ importgp.py
@@ -11,3 +11,18 @@ url = input("Enter the url: ")
 username = input("Enter the username: ")
 password = getpass.getpass("Enter the password: ")
 
+# get nfl sportsbook data
+def get_nfl_info():
+    response = requests.get)(f"{base_url}")
+    return response.json()
+
+# function to write data to a JSON file
+def save_to_json(data, filename):
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+data = {
+    'NFL information': get_nfl_info()
+}
+
+# Get the current date and time
